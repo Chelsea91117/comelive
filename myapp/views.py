417e63f8d63e2f36649f4ad51563a8b8f588c94d
Chelsea91117 <aidana.latifova@yandex.kz>
2024-09-10 22:07:19 +0200

    Created LoginView and LogoutView

--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,60 @@
 from rest_framework import status
+from rest_framework.permissions import AllowAny
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveUpdateDestroyAPIView
-
+from rest_framework.views import APIView
+from django.contrib.auth import authenticate
 from myapp.models import User
 from myapp.serializers import UserCreateUpdateSerializer, UserListSerializer, UserRetrieveUpdateDestroySerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+from datetime import datetime
+
+class LoginView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        email = request.data.get('email')
+        password = request.data.get('password')
+        user = authenticate(request, email=email, password=password)
+
+        if user:
+            refresh = RefreshToken.for_user(user)
+            access_token = refresh.access_token
+
+            # Используем exp для установки времени истечения куки
+            access_expiry = datetime.utcfromtimestamp(access_token['exp'])
+            refresh_expiry = datetime.utcfromtimestamp(refresh['exp'])
+
+            response = Response(status=status.HTTP_200_OK)
+            response.set_cookie(
+                key='access_token',
+                value=str(access_token),
+                httponly=True,
+                secure=False, # Используйте True для HTTPS
+                samesite='Lax',
+                expires=access_expiry
+            )
+            response.set_cookie(
+                key='refresh_token',
+                value=str(refresh),
+                httponly=True,
+                secure=False,
+                samesite='Lax',
+                expires=refresh_expiry
+            )
+            return response
+        else:
+            return Response({"detail": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+class LogoutView(APIView):
+
+    def post(self, request, *args, **kwargs):
+        response = Response(status=status.HTTP_204_NO_CONTENT)
+        response.delete_cookie('access_token')
+        response.delete_cookie('refresh_token')
+        return response
 
 
 class UserRegisterGenericView(CreateAPIView):
