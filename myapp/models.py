4ae980b3f38db89806776e824da0779b90f1da71
Chelsea91117 <aidana.latifova@yandex.kz>
2024-09-10 13:34:59 +0200

    Created model User

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,54 @@
+from django.contrib.auth.models import UserManager, PermissionsMixin, AbstractBaseUser
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class CustomUserManager(UserManager):
+    def create_user(self, email=None, password=None, **extra_fields):
+        if not email:
+            raise ValueError('You didn\'t enter an email address')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=20, blank=True, default='')
+    is_renter = models.BooleanField(default=False)
+    is_landlord = models.BooleanField(default=False)
+
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    date_joined = models.DateTimeField(default=timezone.now)
+    last_login = models.DateTimeField(blank=True, null=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+
+    def get_full_name(self):
+        return self.name
+
+    def get_short_name(self):
+        return self.name or self.email.split('@')[0]
+
+
